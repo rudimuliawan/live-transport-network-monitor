8c13248039ea283e5fa713d7b3f39d6ae53589d2
Rudi Muliawan <muliawan.rudi91@gmail.com>
2024-02-22 10:44:02 +0700

    "Add code for the Boost Asio crash course"

--- network-monitor/src/main.cpp
+++ network-monitor/src/main.cpp
@@ -1,22 +1,60 @@
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
+#include <iomanip>
 #include <iostream>
+#include <thread>
 
 using tcp = boost::asio::ip::tcp;
 
+void Log(boost::system::error_code ec)
+{
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+              << (ec ? "Error: " : "OK")
+              << (ec ? ec.message() : "")
+              << std::endl;
+}
+
+void OnConnect(boost::system::error_code ec)
+{
+    Log(ec);
+}
+
 int main()
 {
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main"
+              << std::endl;
+
     boost::asio::io_context ioc {};
 
     tcp::socket socket {ioc};
 
+    size_t nThreads {4};
+
     boost::system::error_code ec {};
+    tcp::resolver resolver {ioc};
+    auto resolverIt {resolver.resolve("google.com", "80", ec)};
     if (ec) {
-        std::cerr << "Error: " << ec.message() << std::endl;
+        Log(ec);
         return -1;
-    } else {
-        std::cout << "OK" << std::endl;
-        return 0;
     }
+
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        socket.async_connect(*resolverIt, OnConnect);
+    }
+
+    std::vector<std::thread> threads {};
+    threads.reserve(nThreads);
+
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        threads.emplace_back([&ioc]() {
+           ioc.run();
+        });
+    }
+
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        threads[idx].join();
+    }
+
+    return 0;
 }
